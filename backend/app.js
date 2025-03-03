c21e3fc7bb63b9d939434e1e57a497432bf4604a
Adarsh Kumar <adarshkumar.mnnit@gmail.com>
2025-03-03 17:33:48 +0530

    Added productModel, controller & routes

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const cookieParser=require('cookie-parser')
-const dotenv = require('dotenv');
-const globalErrorHandler = require('./controllers/errorController');
-const AppError = require('./utils/appError');
-
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+const globalErrorHandler = require("./controllers/errorController");
+const productRouter = require("./routes/productRoute");
+const AppError = require("./utils/appError");
 
 // Initialize Express app
 const app = express();
@@ -15,15 +15,22 @@ dotenv.config();
 app.use(cookieParser());
 
 // Body parser middleware to read data from body into req.body
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: "10kb" }));
 
-// 404 handler for undefined routes
-app.use('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  console.log("Body:", req.body); // Check if body is undefined
+  next();
 });
 
+app.use("/api/v1", productRouter);
+
+// 404 handler for undefined routes
+app.use("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
 
 // Global error handler middleware
 app.use(globalErrorHandler);
 
-module.exports = app;  // Export app for use in the server setup
+module.exports = app; // Export app for use in the server setup
