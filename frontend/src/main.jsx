0c0cf91ef48e2518c13ab6e64f2af4d8b81d174e
Abhay Sharma <abhaysharma21102006@gmail.com>
2025-03-12 14:57:18 +0530

    updated main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { UserDataProvider } from './store/userDataStore.jsx'
+import { ProductsDataProvider } from './store/productDataStore.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
