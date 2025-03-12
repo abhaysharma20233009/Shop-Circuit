70c8d088df8811036daf193d2c3c7fdfc3bbc288
Abhay Sharma <abhaysharma21102006@gmail.com>
2025-03-12 15:11:10 +0530

    updated main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,12 @@ import { ProductsDataProvider } from './store/productDataStore.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ProductsDataProvider>
+   
     <App />
+    
+    </ProductsDataProvider>
+    
+  
   </StrictMode>,
 )
