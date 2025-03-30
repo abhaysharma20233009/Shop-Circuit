f8f9f95012fe53149b74b547fc74671502fdc415
Abhinav Yadav <abhinav.20233011@mnnit.ac.in>
2025-03-30 16:10:16 +0530

    add easy navigate and links

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
+  const navigate = useNavigate();
   return (
     <footer className="bg-gradient-to-r from-[#03001e] via-[#1e0c2b] to-[#190213] text-white py-10 mt-10 relative overflow-hidden">
       <div className="container mx-auto px-6 grid grid-cols-1 md:grid-cols-3 gap-8 relative z-10">
@@ -18,10 +20,10 @@ const Footer = () => {
         <div className="backdrop-blur-lg bg-white/10 p-6 rounded-xl shadow-md border border-gray-900">
           <h2 className="text-lg font-semibold text-gray-300">Quick Links</h2>
           <ul className="mt-2 space-y-2">
-            <li><a href="#" className="hover:text-purple-400 transition-all duration-300">Home</a></li>
-            <li><a href="#" className="hover:text-purple-400 transition-all duration-300">Products</a></li>
-            <li><a href="#" className="hover:text-purple-400 transition-all duration-300">Rent Items</a></li>
-            <li><a href="#" className="hover:text-purple-400 transition-all duration-300">Contact Us</a></li>
+            <li><a href="/dashboard" className="hover:text-purple-400 transition-all duration-300" >Home</a></li>
+            <li><a href="/sells" className="hover:text-purple-400 transition-all duration-300">Sells</a></li>
+            <li><a href="rents" className="hover:text-purple-400 transition-all duration-300">Rents</a></li>
+            <li><a href="contact" className="hover:text-purple-400 transition-all duration-300">Contact Us</a></li>
           </ul>
         </div>
 
@@ -31,7 +33,7 @@ const Footer = () => {
           <div className="flex space-x-4 mt-3">
             <a href="#" className="text-gray-300 hover:text-purple-400 transition-all duration-300"><FaFacebook size={24} /></a>
             <a href="#" className="text-gray-300 hover:text-purple-400 transition-all duration-300"><FaTwitter size={24} /></a>
-            <a href="#" className="text-gray-300 hover:text-purple-400 transition-all duration-300"><FaInstagram size={24} /></a>
+            <a href="https://www.instagram.com/abhi9.acb/" target="_blank" className="text-gray-300 hover:text-purple-400 transition-all duration-300"><FaInstagram size={24} /></a>
             <a href="#" className="text-gray-300 hover:text-purple-400 transition-all duration-300"><FaLinkedin size={24} /></a>
           </div>
           <p className="mt-3 text-gray-300">Email: support@shopcircuit.com</p>
