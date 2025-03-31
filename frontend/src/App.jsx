473c1d6672888bfb8d20722301f77b04bf8eda2c
Abhay Sharma <abhaysharma21102006@gmail.com>
2025-04-01 02:30:15 +0530

    added other frontend routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,9 @@ import ContactPage from "./components/Contact.jsx";
 import ServicesPage from "./components/Services.jsx";
 import AboutPage from "./components/About.jsx";
 import { ToastContainer } from "react-toastify";
-
+import ForgotPassword from "./pages/account-setting/forgotPassword.jsx";
+import AccountSettings from "./pages/account-setting/account-setting.jsx";
+import ResetPassword from "./pages/account-setting/resetPassword.jsx";
 //  Dashboard Layout
 const DashboardLayout = ({ children }) => (
   <>
@@ -107,6 +109,14 @@ function App() {
                 </ProfileLayout>
               }
             />
+             <Route
+              path="/account-settings"
+              element={
+                <ProfileLayout>
+                  <AccountSettings />
+                </ProfileLayout>
+              }
+            />
             <Route
               path="/chat"
               element={
@@ -130,6 +140,23 @@ function App() {
               element={
                 <AuthLayout>
                   <LoginPage />
+                </AuthLayout>
+              }
+            />
+             <Route
+              path="/forgot-password"
+              element={
+                <AuthLayout>
+                  <ForgotPassword />
+                </AuthLayout>
+              }
+            />
+
+<Route
+              path="/resetPassword/:token"
+              element={
+                <AuthLayout>
+                  <ResetPassword />
                 </AuthLayout>
               }
             />
