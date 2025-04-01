c65b0efb2893262b3f2a0bb800702b24a3f20b23
Abhay Sharma <abhaysharma21102006@gmail.com>
2025-04-01 19:00:38 +0530

    adeed feature  to delete contact query as admin

--- backend/controllers/contactQueryController.js
+++ backend/controllers/contactQueryController.js
@@ -1,10 +1,14 @@
 const  ContactQuery =require( "../models/contactQueryModel.js");
+const User = require("../models/userModel.js");
 
 // Create a new contact query
 exports.createQuery = async (req, res) => {
   try {
-    const { name, email, message } = req.body;
-    const newQuery = await ContactQuery.create({ name, email, message });
+    const id=req.user._id;
+    const user=await User.findById(id);
+    console.log(user+"user"+user.username);
+    const { message } = req.body;
+    const newQuery = await ContactQuery.create({ name:user.username, email:user.email, message });
     res.status(201).json(newQuery);
   } catch (error) {
     res.status(500).json({ error: "Failed to create contact query" });
@@ -20,6 +24,24 @@ exports.getAllQueries = async (req, res) => {
     res.status(500).json({ error: "Failed to fetch contact queries" });
   }
 };
+
+
+exports.deleteContactQuery = async (req, res) => {
+    try {
+      const { id } = req.params;
+  
+      const query = await ContactQuery.findById(id);
+      if (!query) {
+        return res.status(404).json({ message: "Query not found" });
+      }
+  
+      await ContactQuery.findByIdAndDelete(id);
+      res.status(200).json({ message: "Query deleted successfully" });
+    } catch (error) {
+      console.error("Delete Error:", error);
+      res.status(500).json({ message: "Server error" });
+    }
+  };
 
 // Get a single query by ID
 exports.getQueryById = async (req, res) => {
