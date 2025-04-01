dff87c241cf31bd6c9108c25690d7c22047fb844
Adarsh Kumar <adarshkumar.mnnit@gmail.com>
2025-04-01 17:26:14 +0530

    Remove .history from repository

--- .history/backend/app_20250303130605.js
+++
@@ -1,6 +0,0 @@
-const cors = require('cors');
-
-app.use(cors({
-  origin: 'http://localhost:5173', // Adjust based on frontend URL
-  credentials: true, // Allows cookies to be sent with requests
-}));
