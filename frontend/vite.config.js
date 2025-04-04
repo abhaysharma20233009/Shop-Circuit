2abce8387df4f6b7ebbbb9edd2982d449ea17aee
abhaysharma20233009 <147267290+abhaysharma20233009@users.noreply.github.com>
2025-04-04 19:34:56 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
     port: 5173,
     proxy: {
       "/api/v1": {
-        target: "http://localhost:3000",
+        target: "https://shop-circuit.onrender.com",
         changeOrigin: true,
         secure: false,
       },
